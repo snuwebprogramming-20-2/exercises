7bc5f7f2199d5582e31ff9de99622d064aa949a1
blindjh <blindblue@snu.ac.kr>
2020-09-16 20:17:59 +0900

    Update 0916.js

--- 0916.js
+++ 0916.js
@@ -47,3 +47,17 @@ function showInformationOfUsers(arr) {
     }
 }
 showInformationOfUsers(users);
+
+
+const showStar = (n) => {
+    if(n % 2 === 0) {
+        console.log('n is even number.');
+        return;
+    }
+    
+    for(i = 1; i <= n; i = i + 2) {
+        const space = ' '.repeat((n - i) / 2);
+        let line = `${space}${'*'.repeat(i)}${space}`;
+        console.log(line);
+    }
+}
