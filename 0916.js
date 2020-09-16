0dd1595ac03d9c2a9f1584cda8e8d2cfa850134e
blindjh <blindblue@snu.ac.kr>
2020-09-16 20:49:19 +0900

    Update 0916.js

--- 0916.js
+++ 0916.js
@@ -61,3 +61,17 @@ const showStar = (n) => {
         console.log(line);
     }
 }
+
+
+
+const cb = (str) => console.log(str + '!!!')
+
+function a(callback) {
+    console.log('hi');
+    console.log('hello');
+    console.log( 3 + 5);
+
+    callback('yureka');
+}
+
+a(cb);
